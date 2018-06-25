ceaa7f8adf6f9de1d3fa41385ad6e7b8aba9c6b4
jodecile84 <jodecile84@gmail.com>
2018-06-25 12:56:43 -0700

    update navbar

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -8,38 +8,19 @@ class NavBar extends Component {
 
     <div>
 
-    {/* <!-- Dropdown Structure --> */}
-      <ul id="dropdown1" className="dropdown-content">
-        <li><a href="#!">one</a></li>
-        <li><a href="#!">two</a></li>
-        <li className="divider"></li>
-        <li><a href="#!">three</a></li>
-      </ul>
-
       <nav>
         <div className="nav-wrapper">
-          <a href="#!" className="brand-logo">Logo</a>
+          <a href="#!" className="lol-logo"><img src="http://img11.hostingpics.net/thumbs/mini_130684LOLico.png" alt="logo" />
+          <b>CLASH</b>COMP</a>
           <ul className="right hide-on-med-and-down">
-            <li><a href="sass.html">Settings</a></li>
-            <li><a href="badges.html">Profile</a></li>
+            <li><a href="sass.html">SETTINGS</a></li>
+            <li><a href="badges.html">PROFILE</a></li>
+            <li><span id="line"></span></li>
             {/* <!-- Dropdown Trigger --> */}
             <li><a id="button" className="waves-effect waves-light btn">Button</a></li>
           </ul>
         </div>
       </nav>
-
-      {/* <!-- Dropdown Trigger -->
-      <a className='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
-
-      <!-- Dropdown Structure -->
-      <ul id='dropdown1' className='dropdown-content'>
-        <li><a href="#!">one</a></li>
-        <li><a href="#!">two</a></li>
-        <li className="divider" tabindex="-1"></li>
-        <li><a href="#!">three</a></li>
-        <li><a href="#!"><i className="material-icons">view_module</i>four</a></li>
-        <li><a href="#!"><i className="material-icons">cloud</i>five</a></li>
-      </ul> */}
 
     </div>
     );
